5adcd5d8a6d58d6105386d84f939df7bdd954433
vermeiren <vermeiren.remy@gmail.com>
2016-11-14 18:42:53 +0100

    header with translation done

--- header.php
+++ header.php
@@ -35,50 +35,30 @@
 
 		<div class="site-branding">
 			<div class="wrap">
-
-				<?php
-				$header_image = get_header_image();
-				if ( ! empty( $header_image ) ) : ?>
-					<!-- <div class="header-image-contain"> -->
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-							<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width +1000); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="" class="header-image">
-						</a>
+				<div class="header-container">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<?php if (ICL_LANGUAGE_CODE == 'fr') echo '<img src="wp-content/themes/wecitizens_new/images/header-fr.jpg" width="100%" height="auto">'; ?>
+						<?php if (ICL_LANGUAGE_CODE == 'en') echo '<img src="wp-content/themes/wecitizens_new/images/header-en.jpg" width="100%" height="auto">'; ?>
+						<?php if (ICL_LANGUAGE_CODE == 'nl') echo '<img src="wp-content/themes/wecitizens_new/images/header-nl.jpg" width="100%" height="auto">'; ?>
+					</a>
+					<nav id="languages">
+						<ul>
+						<?php
+						if (function_exists('icl_get_languages')) {
+						    $languages = icl_get_languages('skip_missing=0');
+						    if (1 < count($languages)) {
+						        foreach ($languages as $l) {
+						            echo '<li><a href="' . $l['url'] . '"><img src="' . $l['country_flag_url'] . '" alt="' . $l['language_code'] . '" /></a></li>';
+						        }
+						    }
+						}
+						?>
+						</ul>
+					</nav>
+				</div>
 
 					<!-- </div>--><!-- .header-image -->
-				<?php endif;
 
-				scratchpad_the_site_logo();
-
-				if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-				endif;
-
-				$description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) : ?>
-					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-				<?php endif; ?>
-				<nav id="languages">
-					<ul>
-					<?php
-					if (function_exists('icl_get_languages')) {
-					    $languages = icl_get_languages('skip_missing=0');
-					    if (1 < count($languages)) {
-					        foreach ($languages as $l) {
-					            echo '<li><a href="' . $l['url'] . '"><img src="' . $l['country_flag_url'] . '" alt="' . $l['language_code'] . '" /></a></li>';
-					        }
-					    }
-					}
-					?>
-					<!--
-					<li><a href="<?php echo site_url(); ?>"><img src="<?php echo plugins_url(); ?>/sitepress-multilingual-cms/res/flags/en.png" /></a></li>
-					<li><a href="<?php echo site_url(); ?>/fr/"><img src="<?php echo plugins_url(); ?>/sitepress-multilingual-cms/res/flags/fr.png" /></a></li>
-					<li><a href="<?php echo site_url(); ?>/nl/"><img src="<?php echo plugins_url(); ?>/sitepress-multilingual-cms/res/flags/nl.png" /></a></li>
-					-->
-					</ul>
-				</nav>
 			</div><!-- .wrap -->
 		</div><!-- .site-branding -->
 
