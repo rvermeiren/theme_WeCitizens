3fb4b3c5b8742ec4182eb90ad05f37b1079f76a1
xschul <xavier.schul@student.uclouvain.be>
2016-12-01 14:14:21 +0100

    page +css

--- footer.php
+++ footer.php
@@ -16,12 +16,11 @@
 
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-
-		<div></div>
-
-		<!-- lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll -->
+		<!-- User cases footer -->
 
 		<div class="wrap">
+
+			<!--  -->
 
 			<table class="table-footer" border="0"> <!-- FAIRE EN SORTE QUE CA SOIT CONTENU DANS LA LARGEUR DU CONTENT -->
 				<tbody class="tbody-footer">
